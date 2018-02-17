362e734c15f4004bd73b142892f47034dab6965f
AsmodeusXI <AsmodeusXI@gmail.com>
2018-02-16 22:09:24 -0500

    feat: add Twitch player
    Use ReactPlayer module to install Twitch into website!

--- src/content/tabs/twitch.tsx
+++ src/content/tabs/twitch.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import ReactPlayer from 'react-player';
+
+import './twitch.scss';
 
 export class Twitch extends React.Component<{}, {}> {
   render() {
-    return <p>Twitch Viewer currently under construction.</p>
+    return <article id="twitch-tab" className='center-viewer'>
+      <div id="twitch-container">
+        <ReactPlayer url='https://www.twitch.tv/djforeclosure' />
+      </div>
+    </article>;
   }
 }
