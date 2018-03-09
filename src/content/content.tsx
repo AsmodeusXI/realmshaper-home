ab989bfa2af406ad2428206eded712c367da4153
Sam Lawton <sam.h.lawton@gmail.com>
2018-03-08 21:58:15 -0500

    feat: slight organization change

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 
-import { About } from "./tabs/about";
-import { Twitch } from "./tabs/twitch";
-import { NameGen } from "./tabs/namegen";
+import { About } from "./tabs/about/about";
+import { Twitch } from "./tabs/twitch/twitch";
+import { NameGen } from "./tabs/namegen/namegen";
 import { FLCCombat } from "./tabs/combat/flc-combat";
 
 import { GeneratorMain } from "name-maker";
