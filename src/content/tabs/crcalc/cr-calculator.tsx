6f8000b8235d515f86f6a5b46377ef3e8fabf469
Sam Lawton <sam.h.lawton@gmail.com>
2018-04-17 22:29:03 -0400

    feat: set up CR calculator tab for site

---
+++ src/content/tabs/crcalc/cr-calculator.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import * as cr from "dnd-5e-cr-calculator";
+
+import './cr-calculator.scss';
+
+export class CRCalculator extends React.Component<{}, {}> {
+  render() {
+    return <article id="cr-calculator-tab">
+      CR Calculator
+    </article>;
+  }
+}
