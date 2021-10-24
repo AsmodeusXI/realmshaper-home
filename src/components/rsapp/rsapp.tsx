498050c46837bd1ab95def147c5200746392b007
Sam Lawton <sam.h.lawton@gmail.com>
2021-10-24 19:54:59 -0400

    refactor: another small one

--- src/components/rsapp/rsapp.tsx
+++ src/components/rsapp/rsapp.tsx
@@ -3,15 +3,16 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
-    Link
 } from 'react-router-dom';
+
 import { Footer } from '../../footer/footer';
 import { Header } from '../../header/header';
+import RSNav from '../rsnav/rsnav';
+import { RSTab } from './rsapp.types';
+import './rsapp.scss';
+
 import Bio from '../bio/bio';
 import HabitGame from '../habitgame/habitgame';
-import RSNav from '../rsnav/rsnav';
-import './rsapp.scss';
-import { RSTab } from './rsapp.types';
 
 const RSApp = (): JSX.Element => {
     const tabs = [
@@ -20,12 +21,6 @@ const RSApp = (): JSX.Element => {
         { order: 3, location: '/habitgame', title: 'Habit Game', component: <HabitGame name='Habit Game' /> }
     ];
 
-    const pageEntries = tabs
-        .sort((a: RSTab, b: RSTab) => b.order - a.order)
-        .map((tab: RSTab) => {
-            return <Route path={tab.location}><p>{tab.component}</p></Route>
-        });
-
     return (
         <Router>
             <section id="rs-app">
@@ -33,7 +28,13 @@ const RSApp = (): JSX.Element => {
                 <main id="rs-main">
                     <RSNav tabs={tabs} />
                     <section id="rs-page">
-                        <Switch>{pageEntries}</Switch>
+                        <Switch>{
+                            tabs
+                                .sort((a: RSTab, b: RSTab) => b.order - a.order)
+                                .map((tab: RSTab) => {
+                                    return <Route path={tab.location}><p>{tab.component}</p></Route>
+                                })
+                        }</Switch>
                     </section>
                 </main>
                 <Footer />
