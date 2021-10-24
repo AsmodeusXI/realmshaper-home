3e82e074deb81bd26e199524e934ee50d1450ed7
Sam Lawton <sam.h.lawton@gmail.com>
2021-10-24 19:53:10 -0400

    refactor: small nav

--- src/components/rsnav/rsnav.tsx
+++ src/components/rsnav/rsnav.tsx
@@ -4,15 +4,13 @@ import { Link } from 'react-router-dom';
 import { RSTab } from '../rsapp/rsapp.types';
 
 const RSNav = ({ tabs }): JSX.Element => {
-    const navEntries = tabs
-        .sort((a: RSTab, b: RSTab) => a.order - b.order)
-        .map((tab: RSTab) =>
-            <li><Link to={tab.location}>{tab.title}</Link></li>
-        );
-
     return (
         <nav id="rs-nav">
-            <ul>{navEntries}</ul>
+            <ul>{
+                tabs
+                    .sort((a: RSTab, b: RSTab) => a.order - b.order)
+                    .map((tab: RSTab) => <li><Link to={tab.location}>{tab.title}</Link></li>)
+            }</ul>
         </nav>
     )
 }
